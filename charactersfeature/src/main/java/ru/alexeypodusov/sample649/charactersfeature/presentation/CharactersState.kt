7e94a6bcfa1f0b6c45ae2fd6ee891a4447bfcd3a
alexey podusov <31965573+alexeypodusov@users.noreply.github.com>
2022-12-06 00:55:50 +0400

    var to val

--- charactersfeature/src/main/java/ru/alexeypodusov/sample649/charactersfeature/presentation/CharactersState.kt
+++ charactersfeature/src/main/java/ru/alexeypodusov/sample649/charactersfeature/presentation/CharactersState.kt
@@ -2,13 +2,13 @@ package ru.alexeypodusov.sample649.charactersfeature.presentation
 
 import ru.alexeypodusov.sample649.base.util.UiText
 
-sealed class CharactersState(open var isOpenedSearchBar: Boolean) {
-    data class Loading(override var isOpenedSearchBar: Boolean) : CharactersState(isOpenedSearchBar)
+sealed class CharactersState(open val isOpenedSearchBar: Boolean) {
+    data class Loading(override val isOpenedSearchBar: Boolean) : CharactersState(isOpenedSearchBar)
     data class Loaded(
-        override var isOpenedSearchBar: Boolean,
+        override val isOpenedSearchBar: Boolean,
         val characters: List<Character>
     ) : CharactersState(isOpenedSearchBar)
-    data class Empty(override var isOpenedSearchBar: Boolean) : CharactersState(isOpenedSearchBar)
-    data class Error(override var isOpenedSearchBar: Boolean, val errorMessage: UiText) : CharactersState(isOpenedSearchBar)
+    data class Empty(override val isOpenedSearchBar: Boolean) : CharactersState(isOpenedSearchBar)
+    data class Error(override val isOpenedSearchBar: Boolean, val errorMessage: UiText) : CharactersState(isOpenedSearchBar)
 }
 
